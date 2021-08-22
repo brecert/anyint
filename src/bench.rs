e5affb06ac02c7030befffbd7251f66ab038981f
brecert <me@bree.dev>
2021-08-22 05:07:41 -0400

    improve benchmarks to be more comprehensive
    - improved benchmarks to test more methods

--- src/bench.rs
+++ src/bench.rs
@@ -2,30 +2,42 @@ extern crate test;
 use crate::{integer::int, prelude::NonStandardIntegerCommon};
 use test::{black_box, Bencher};
 
-#[bench]
-fn bench_add_5000_u21(b: &mut Bencher) {
-    b.iter(|| {
-        (0..5000).fold(int::<u32, 21>::new(0), |old, new| {
-            black_box(old.wrapping_add(int::<u32, 21>::new(new)))
-        })
-    });
+macro wrapping_bench($method:ident) {
+    mod $method {
+        use super::*;
+        
+        #[bench]
+        fn int_u21(b: &mut Bencher) {
+            b.iter(|| {
+                (1..5000).fold(int::<u32, 21>::new(0), |old, new| {
+                    black_box(old.$method(int::<u32, 21>::new(new)))
+                })
+            });
+        }
+        
+        #[bench]
+        fn std_u32(b: &mut Bencher) {
+            b.iter(|| (1..5000).fold(0, |old: u32, new| black_box(old.$method(new))));
+        }
+        
+        #[bench]
+        fn int_i38(b: &mut Bencher) {
+            b.iter(|| {
+                (1..5000).fold(int::<i64, 38>::new(0), |old, new| {
+                    black_box(old.$method(int::<i64, 38>::new(new)))
+                })
+            });
+        }
+        
+        #[bench]
+        fn std_i64(b: &mut Bencher) {
+            b.iter(|| (1..5000).fold(0, |old: i64, new| black_box(old.$method(new))));
+        }
+    }
 }
 
-#[bench]
-fn bench_add_5000_u32(b: &mut Bencher) {
-    b.iter(|| (0..5000).fold(0, |old: u32, new| black_box(old.wrapping_add(new))));
-}
-
-#[bench]
-fn bench_add_5000_i38(b: &mut Bencher) {
-    b.iter(|| {
-        (0..5000).fold(int::<i64, 38>::new(0), |old, new| {
-            black_box(old.wrapping_add(int::<i64, 38>::new(new)))
-        })
-    });
-}
-
-#[bench]
-fn bench_add_5000_i64(b: &mut Bencher) {
-    b.iter(|| (0..5000).fold(0, |old: i64, new| black_box(old.wrapping_add(new))));
-}
+wrapping_bench!(wrapping_add);
+wrapping_bench!(wrapping_sub);
+wrapping_bench!(wrapping_mul);
+wrapping_bench!(wrapping_div);
+wrapping_bench!(wrapping_rem);
